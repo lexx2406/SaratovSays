dfddc97e5e2cfa283b5a9934fa3913ea212d9404
lexx2406 <lexx2406@yandex.ru>
2023-11-18 05:29:49 +0400

    Создал главный контроллер и первое ветвление

--- SaratovSays/Controllers/ViewController.swift
+++ SaratovSays/Controllers/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Street: UIButton!
+    @IBOutlet weak var Home: UIButton!
+    @IBAction func Street(_ sender: Any) {
+    }
+    @IBAction func Home(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
