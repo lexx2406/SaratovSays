d5bee3b4d0a6af4106218daacad649119144994e
lexx2406 <lexx2406@yandex.ru>
2023-11-18 04:32:47 +0400

    Initial Commit

---
+++ SaratovSays/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaratovSays
+//
+//  Created by Алексей Налимов on 18.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
